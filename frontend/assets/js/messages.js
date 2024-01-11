f3093b4d4f05b3262ce870c88e86ddfa23e4e277
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-01-10 22:13:27 -0300

    fix: order css files - add base and header to global-components

--- frontend/assets/js/messages.js
+++ frontend/assets/js/messages.js
@@ -1,6 +1,7 @@
 export default (type, text) => {
     const message = document.querySelector(".message");
+    message.style.display = "block";
     if(type === "danger") {
-        message.innerText
+        message.innerText = text;
     }
 }
