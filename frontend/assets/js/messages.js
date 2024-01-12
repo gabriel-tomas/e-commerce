1707975ccb2aba934e6144214b5f1da970307f2b
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-01-12 01:52:39 -0300

    fix: animationend listener for message

--- frontend/assets/js/messages.js
+++ frontend/assets/js/messages.js
@@ -4,20 +4,21 @@ const counterAdded = document.querySelector(".message > .counter-added");
 let counter = 1;
 
 export default (type, text) => {
+    message.addEventListener("animationend", () => {
+        message.classList.remove("active");
+    });
     if(message.classList.contains("active")) {
         counter++;
         counterAdded.style.display = "block";
         counterAdded.innerText = counter;
-        message.classList.remove("active");
     } else {
         counterAdded.style.display = "none";
+        message.classList.add("active");
         setTimeout(() => {
-            message.classList.remove("active");
             counter = 1;
         }, 4000);
     }
     messageP.innerText = text;
-    message.classList.add("active");
     if(type === "danger") {
         message.classList.add("danger");
     } else if(type === "success") {
