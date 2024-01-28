29d3ce4cb3812fb439781cdb596f322af4573cac
gabriel-tomas <gabriel.tomas.com@gmail.com>
2024-01-27 23:40:21 -0300

    fix: try catch to MyData and if data is not sent return a error message

--- src/controllers/myData.js
+++ src/controllers/myData.js
@@ -33,22 +33,34 @@ exports.edit = async (req, res) => {
         language =  "en";
     }
 
-    const myData = new MyData(req.session.user._id, req.body, language);
-    await myData.edit();
-    if(myData.errors.length > 0)  {
-        req.flash("errors", myData.errors);
+    if(!req.body) {
+        req.flash("error", language === "ptBr"? "Os dados precisam ser enviados": "Data needs to be sent");
         req.session.save(() => {
             res.redirect("/myData");
         });
-    } else {
-        req.flash("success", language === "ptBr"? "Usuário editado com sucesso": "User edited successfully!");
-        req.session.user.name = myData.user.name;
-        req.session.user.surname = myData.user.surname;
-        req.session.user.email = myData.user.email;
-        req.session.user.password = myData.user.password;
-        req.session.user.userPassword = req.body.password.trim();
-        req.session.save(() => {
-            res.redirect("/myData");
-        });
+        return;
+    }
+
+    try {
+        const myData = new MyData(req.session.user._id, req.body, language);
+        await myData.edit();
+        if(myData.errors.length > 0)  {
+            req.flash("errors", myData.errors);
+            req.session.save(() => {
+                res.redirect("/myData");
+            });
+        } else {
+            req.flash("success", language === "ptBr"? "Usuário editado com sucesso": "User edited successfully!");
+            req.session.user.name = myData.user.name;
+            req.session.user.surname = myData.user.surname;
+            req.session.user.email = myData.user.email;
+            req.session.user.password = myData.user.password;
+            req.session.user.userPassword = req.body.password.trim();
+            req.session.save(() => {
+                res.redirect("/myData");
+            });
+        }
+    } catch(e) {
+        console.log(e);
     }
 }
